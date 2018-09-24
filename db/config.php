de6e3b9dff6c89c82f01258f426e5a26a25096c6
aeiyjl <aei.yjl@gmail.com>
2018-09-24 18:10:40 +0800

    db

--- db/config.php
+++ db/config.php
@@ -5,11 +5,11 @@ $password = "123";
 $dbname = "sysod";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$db = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
 } 
 
 ?>
